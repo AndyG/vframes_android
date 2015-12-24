19f51460434512751e58cc708d89e324c3af1b25
Andy Garron <anderson.garron@gmail.com>
2015-12-23 23:06:52 -0800

    added dhalsim's moveset

--- app/src/main/java/com/angarron/sfvframedata/resource_resolution/StringResolver.java
+++ app/src/main/java/com/angarron/sfvframedata/resource_resolution/StringResolver.java
@@ -17,11 +17,16 @@ public class StringResolver {
 
         //General Movelist
         stringIdMap.put("id_close_to_opponent", R.string.close_to_opponent);
+        stringIdMap.put("id_can_be_done_in_air", R.string.can_be_done_in_air);
+        stringIdMap.put("id_during_jump", R.string.during_jump);
+        stringIdMap.put("id_during_guard", R.string.during_guard);
 
         //Karin Movelist
         stringIdMap.put("id_karin_tenko_posttext", R.string.karin_tenko_posttext);
-        stringIdMap.put("id_yashagaeshi_pretext", R.string.karin_yashagaeshi_pretext);
         stringIdMap.put("id_karin_meioken_posttext", R.string.karin_meioken_posttext);
+
+        //Dhalsim Movelist
+        stringIdMap.put("id_dhalsim_gale_pretext", R.string.dhalsim_gale_pretext);
     }
 
     public static int getStringId(String key) {
