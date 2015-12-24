bce4f2229d888ce09018b0950b7a61bfb5d31a78
Andy Garron <anderson.garron@gmail.com>
2015-12-23 23:36:52 -0800

    added claw movelist

--- app/src/main/java/com/angarron/sfvframedata/resource_resolution/StringResolver.java
+++ app/src/main/java/com/angarron/sfvframedata/resource_resolution/StringResolver.java
@@ -19,7 +19,10 @@ public class StringResolver {
         stringIdMap.put("id_close_to_opponent", R.string.close_to_opponent);
         stringIdMap.put("id_can_be_done_in_air", R.string.can_be_done_in_air);
         stringIdMap.put("id_during_jump", R.string.during_jump);
+        stringIdMap.put("id_during_jump_near_wall", R.string.during_jump_near_wall);
+        stringIdMap.put("id_during_forward_jump", R.string.during_forward_jump);
         stringIdMap.put("id_during_guard", R.string.during_guard);
+        stringIdMap.put("id_hold_buttons_to_attack", R.string.hold_buttons_to_attack);
 
         //Karin Movelist
         stringIdMap.put("id_karin_tenko_posttext", R.string.karin_tenko_posttext);
@@ -27,6 +30,11 @@ public class StringResolver {
 
         //Dhalsim Movelist
         stringIdMap.put("id_dhalsim_gale_pretext", R.string.dhalsim_gale_pretext);
+
+        //Claw Movelist
+        stringIdMap.put("id_claw_claw_on_only", R.string.claw_claw_on_only);
+        stringIdMap.put("id_claw_claw_off_only", R.string.claw_claw_off_only);
+        stringIdMap.put("id_claw_switch", R.string.claw_switch);
     }
 
     public static int getStringId(String key) {
